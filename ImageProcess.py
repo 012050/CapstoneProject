8c7bfef97854b643def4947a4c85a5c6639ac463
012050 <beomjunchoe@naver.com>
2023-07-22 07:18:09 +0900

    사진 전송 및 번호판 추출

---
+++ ImageProcess.py
@@ -0,0 +1,46 @@
+import os, cv2, easyocr, re
+import numpy as np
+
+test = [
+    '154러7070',
+    '265버9344',
+    '123가4568',
+    '21고7824',
+    '33너2222'
+]
+
+reader = easyocr.Reader(['ko'], gpu = True)
+
+def save_img(file, name, img_dir="Images"):
+	nparr = np.fromstring(file.read(), np.uint8)
+	img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+	if not os.path.isdir(img_dir):
+		os.mkdir(img_dir)
+	cv2.imwrite(os.path.join(img_dir,str(name)+".jpg"), img)
+
+def read_img(board):
+	result = None
+	results = reader.readtext(f"Images/{board}")
+	if results:
+		pattern1 = r'\d{2}[가-힣]\s\d{4}'
+		pattern2 = r'\d{3}[가-힣]\s\d{4}'
+
+		text =str(results[0][1])
+
+		match1 = re.search(pattern1, text)
+		match2 = re.search(pattern2, text)
+
+		if match1 or match2:
+			if match1:
+				matched_text = str(match1.group())
+			else:
+				matched_text = str(match2.group())
+			print(matched_text)
+		else:
+			print(text)
+		matched_text = matched_text.replace(" ", "")
+		if matched_text in test:
+			result = 1
+		else:
+			result = 0
+	return result
--- app.py
+++ app.py
@@ -1,20 +1,26 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request
+from ImageProcess import save_img, read_img
 
 app = Flask(__name__)
 
-# @app.route("/object_detection/<userdevice>")
-@app.route("/device/<board>", methods=['GET', 'POST'])
-def object_detection(board):
-    return board
+# 사진을 저장할 폴더
+img_dir = "Images"
 
-# @app.route("/device/<board>", methods=['POST'])
-# def count(board):
-#     data = request.get_json()
-#     print(data)
-#     count = data.get('count')
-#     fin = data.get('check')
-#     if fin == "fin":
-#         print("데이터 전송 완료")
-#     else:
-#         print(f"\n\n\n\n{count}")
-#     return jsonify({'message': 'Success'})
+UPLOAD_FOLDER = img_dir
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route('/upload/<board>', methods=['POST'])
+def upload_photo(board):
+    received = request
+    result = 0
+    if received.files:
+        file  = received.files['imageFile']
+        save_img(file, board, img_dir=app.config['UPLOAD_FOLDER'])
+        result = read_img(board)
+        print(f"[SUCCESS] Image Received, result: {result}")
+        # return f"[SUCCESS] Image Received, result: {result}", 201
+    else:
+        print("[FAILED] Image Not Received")
+        return "[FAILED] Image Not Received", 204
+
+    return result, 200
