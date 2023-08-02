4896fed692ea09f3b64282a55883650161806792
012050 <101517763+012050@users.noreply.github.com>
2023-08-03 07:40:22 +0900

    카메라 핀 맵 파일 추가

--- client_image_post/client_image_post.ino
+++ client_image_post/client_image_post.ino
@@ -16,6 +16,28 @@
 #include "esp_camera.h"
 #include <ESP32Servo.h>
 
+// // CAMERA_MODEL_AI_THINKER
+// #define PWDN_GPIO_NUM     32
+// #define RESET_GPIO_NUM    -1
+// #define XCLK_GPIO_NUM      0
+// #define SIOD_GPIO_NUM     26
+// #define SIOC_GPIO_NUM     27
+
+// #define Y9_GPIO_NUM       35
+// #define Y8_GPIO_NUM       34
+// #define Y7_GPIO_NUM       39
+// #define Y6_GPIO_NUM       36
+// #define Y5_GPIO_NUM       21
+// #define Y4_GPIO_NUM       19
+// #define Y3_GPIO_NUM       18
+// #define Y2_GPIO_NUM        5
+// #define VSYNC_GPIO_NUM    25
+// #define HREF_GPIO_NUM     23
+// #define PCLK_GPIO_NUM     22
+
+#define CAMERA_MODEL_AI_THINKER
+#include "camera_pins.h"
+
 const char* ssid = "DESKTOP-N75BSJP 8172";
 const char* password = "2@6A6o73";
 
@@ -23,25 +45,6 @@ String serverName = "minimalist.iptime.org";
 String serverPath = "/upload/boardid";  // Flask upload route
 const int serverPort = 14523;
 WiFiClient client;
-
-// CAMERA_MODEL_AI_THINKER
-#define PWDN_GPIO_NUM     32
-#define RESET_GPIO_NUM    -1
-#define XCLK_GPIO_NUM      0
-#define SIOD_GPIO_NUM     26
-#define SIOC_GPIO_NUM     27
-
-#define Y9_GPIO_NUM       35
-#define Y8_GPIO_NUM       34
-#define Y7_GPIO_NUM       39
-#define Y6_GPIO_NUM       36
-#define Y5_GPIO_NUM       21
-#define Y4_GPIO_NUM       19
-#define Y3_GPIO_NUM       18
-#define Y2_GPIO_NUM        5
-#define VSYNC_GPIO_NUM    25
-#define HREF_GPIO_NUM     23
-#define PCLK_GPIO_NUM     22
 
 // 초음파
 #define echoPin 14
