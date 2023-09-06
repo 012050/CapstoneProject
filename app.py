b092fd315fb588ea157343ca6b81108cc38e1840
012050 <beomjunchoe@naver.com>
2023-09-06 15:43:14 +0900

    모터 테스트 코드

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request
-from ImageProcess import save_img, read_img
+from ImageProcess import *
 
 app = Flask(__name__)
+
+test_code = "hello world"
 
 # 사진을 저장할 폴더
 img_dir = "Images"
@@ -23,4 +25,21 @@ def upload_photo(board):
         print("[FAILED] Image Not Received")
         return "[FAILED] Image Not Received", 204
 
-    return str(result), 200
+    return str(test_code)
+    # return str(result), 200
+
+@app.route('/test/<code>')
+def upload_photo(code):
+    global test_code
+    if code == "on":
+        test_code = "hello world"
+    elif code == "off":
+        test_code = "bye world"
+
+    print(test_code)
+    return str(test_code)
+
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000, debug=True)
