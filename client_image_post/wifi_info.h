87d9627c576476e3705da571fcb411f20ec93dae
012050 <beomjunchoe@naver.com>
2023-11-06 01:58:46 +0900

    통신 관련 데이터 파일

---
+++ client_image_post/wifi_info.h
@@ -0,0 +1,11 @@
+
+#if defined(WIFI_INFO)
+#define WIFI_ID_INFO   "wifi_name"
+#define WIFI_PW_INFO   "wifi_password"
+
+#define SERVER_ADDRESS   "test.com"
+#define SERVER_PORT      8080
+
+#else
+#error "Wifi info do not defined"
+#endif
